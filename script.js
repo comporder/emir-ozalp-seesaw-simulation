b0bebc6041e87495ad170ba2d026c287b9991667
Emir Ozalp <emirozalpp@gmail.com>
2026-01-12 17:42:31 +0300

    add random weights to seesaw

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 const seesaw = document.getElementById("seesaw");
+const scene = document.getElementById("scene");
 let totalWeightOnLeft = 0;
 let totalWeightOnRight = 0;
 const distanceFromPivot = 1;
 
+function generateRandomWeight() {
+    return Math.floor(Math.random() * 10 + 1);
+}
 
-seesaw.addEventListener("click", function (event) {
-    const seesawBounds = seesaw.getBoundingClientRect();
-    const seesawCenter = seesawBounds.left + seesawBounds.width / 2;
-
-    if (event.clientX < seesawCenter) {
-        totalWeightOnLeft += 1;
-        console.log("Total weight on left:", totalWeightOnLeft);
-    } else {
-        totalWeightOnRight += 1;
-        console.log("Total weight on right:",  totalWeightOnRight);
-    }
-
-    const angle = calculateSeesawAngle();
-    seesaw.style.transform = `rotate(${angle}deg)`;
-});
+function createWeightElement(weight) {
+    const weightElement = document.createElement("div");
+    weightElement.className = "weight";
+    weightElement.textContent = weight;
+    return weightElement;
+}
 
 function calculateSeesawAngle() {
     const leftTorque = totalWeightOnLeft * distanceFromPivot;
     const rightTorque = totalWeightOnRight * distanceFromPivot;
 
     if (leftTorque > rightTorque) {
-        return -15;
+        return -30;
     } else if (rightTorque > leftTorque) {
-        return 15;
+        return 30;
     } else {
         return 0;
     }
 }
+
+seesaw.addEventListener("click", function (event) {
+    const seesawBounds = seesaw.getBoundingClientRect();
+    const seesawCenter = seesawBounds.left + seesawBounds.width / 2;
+    const weight = generateRandomWeight();
+    const weightElement = createWeightElement(weight);
+
+    if (event.clientX < seesawCenter) {
+        totalWeightOnLeft += weight;
+        weightElement.style.left = "30%";
+
+        console.log("Total weight on left:", totalWeightOnLeft);
+    } else {
+        totalWeightOnRight += weight;
+        weightElement.style.left = "70%";
+        console.log("Total weight on right:", totalWeightOnRight);
+    }
+
+    scene.appendChild(weightElement);
+
+    const angle = calculateSeesawAngle();
+    seesaw.style.transform = `rotate(${angle}deg)`;
+});
+
+
+
+
 

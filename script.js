6cb009f3ff731b488ae5901d4a491427a76096c9
Emir Ozalp <emirozalpp@gmail.com>
2026-01-12 16:21:29 +0300

    add basic torque logic

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const seesaw = document.getElementById("seesaw");
+let totalWeightOnLeft = 0;
+let totalWeightOnRight = 0;
+const distanceFromPivot = 1;
 
-seesaw.addEventListener("click", function () {
-  console.log("clicked");
+
+seesaw.addEventListener("click", function (event) {
+    const seesawBounds = seesaw.getBoundingClientRect();
+    const seesawCenter = seesawBounds.left + seesawBounds.width / 2;
+
+    if (event.clientX < seesawCenter) {
+        totalWeightOnLeft += 1;
+        console.log("Total weight on left:", totalWeightOnLeft);
+    } else {
+        totalWeightOnRight += 1;
+        console.log("Total weight on right:",  totalWeightOnRight);
+    }
+
+    const angle = calculateSeesawAngle();
+    seesaw.style.transform = `rotate(${angle}deg)`;
 });
+
+function calculateSeesawAngle() {
+    const leftTorque = totalWeightOnLeft * distanceFromPivot;
+    const rightTorque = totalWeightOnRight * distanceFromPivot;
+
+    if (leftTorque > rightTorque) {
+        return -15;
+    } else if (rightTorque > leftTorque) {
+        return 15;
+    } else {
+        return 0;
+    }
+}
+
